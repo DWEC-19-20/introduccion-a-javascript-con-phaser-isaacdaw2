026825d5a26e5d6f2b6f5decf5e6e4e626102a4f
Isaac Taveras <taveras1989@hotmail.com>
2019-09-26 12:09:44 +0200

    juego terminado

--- game.js
+++ game.js
@@ -11,23 +11,63 @@ var winningMessage;
 var won = false;
 var currentScore = 0;
 var winningScore = 100;
+var veneno;
+var estrella;
 
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin');
+  veneno = game.add.physicsGroup();
+  estrella = game.add.physicsGroup();
+  
+  createItem(570, 130, 'coin');
+  createItem(230, 200, 'coin');
+  createItem(100, 240, 'coin');
+  createItem(100, 240, 'coin');
+  createItem(650, 240, 'coin');
+  createItem(520, 300, 'coin');
+  createItem(380, 400, 'coin');
+  createItem(220, 500, 'coin');
+  createItem(600, 500, 'coin');
+
+  createPoison(380, 80, 'poison');
+  createPoison(370, 500, 'poison');
+
+  createStar(130, 40, 'star');
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(450, 150, 'platform');
+  platforms.create(250, 130, 'platform');
+  platforms.create(545, 180, 'platform2');
+  platforms.create(150, 250, 'platform');
+  platforms.create(650, 290, 'platform');
+  platforms.create(50, 295, 'platform');
+  platforms.create(400, 350, 'platform2');
+  platforms.create(310, 450, 'platform2');
+  platforms.create(100, 550, 'platform');
+  platforms.create(480, 550, 'platform');
+
+  platforms.create(100, 80, 'platform2');
+
   platforms.setAll('body.immovable', true);
 }
 
 // create a single animated item and add to screen
 function createItem(left, top, image) {
   var item = items.create(left, top, image);
+  item.animations.add('spin');
+  item.animations.play('spin', 10, true);
+}
+
+function createPoison(left, top, image) {
+  var item = veneno.create(left, top, image);
+  item.animations.add('spin');
+  item.animations.play('spin', 10, true);
+}
+function createStar(left, top, image) {
+  var item = estrella.create(left, top, image);
   item.animations.add('spin');
   item.animations.play('spin', 10, true);
 }
@@ -46,7 +86,16 @@ function itemHandler(player, item) {
   currentScore = currentScore + 10;
   if (currentScore === winningScore) {
       createBadge();
-  }
+  } 
+}
+
+function poisonHandler(player, veneno) {
+  veneno.kill();
+  currentScore = currentScore - 10; 
+}
+function starHandler(player, estrella) {
+  estrella.kill();
+  currentScore = currentScore + 50;
 }
 
 // when the player collects the badge at the end of the game
@@ -65,11 +114,14 @@ window.onload = function () {
     
     //Load images
     game.load.image('platform', 'platform_1.png');
+    game.load.image('platform2', 'platform_2.png');
     
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
     game.load.spritesheet('coin', 'coin.png', 36, 44);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('star', 'star.png', 32, 32);
   }
 
   // initial game set up
@@ -96,6 +148,8 @@ window.onload = function () {
     text.text = "SCORE: " + currentScore;
     game.physics.arcade.collide(player, platforms);
     game.physics.arcade.overlap(player, items, itemHandler);
+    game.physics.arcade.overlap(player, veneno, poisonHandler);
+    game.physics.arcade.overlap(player, estrella, starHandler);
     game.physics.arcade.overlap(player, badges, badgeHandler);
     player.body.velocity.x = 0;
 
